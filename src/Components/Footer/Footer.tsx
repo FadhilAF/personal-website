f76d801d5eece21ec3947d3f1c2ef2b57b2ed3a8
FadhilAF <falfatih3421@gmail.com>
2022-05-03 11:53:00 +0700

    add content to footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,11 +1,23 @@
 import styles from './Footer.module.css';
 import Wave from './Wave/Wave';
+import logo from '../../Assets/logo.svg';
 
 function Footer() {
     return (
         <>
             <Wave />
-            <div className={styles.infos}></div>
+            <div className={styles.infos}>
+                <img className={styles.logo} src={logo} alt="Logo Website" />
+                <div className={styles['about-page']}>
+                    <p className={styles.title}>About this Website</p>
+                    The first purpose of this wesite is for me to learn react and express.
+                    And the second one is to share information that I want to share. Lastly,
+                    to give authentication to some special people.
+                </div>
+                <div className={styles['about-me']}>
+                    Fuck fuck 2
+                </div>
+            </div>
         </>
     )
 }
--- src/Components/ToggleSwitch/ToggleSwitch.tsx
+++ src/Components/ToggleSwitch/ToggleSwitch.tsx
@@ -5,23 +5,12 @@ const ToggleSwitch = (props: { label: string; theme: {theme: string; setTheme: a
   const [checked, setChecked] = useState(false);
 
   useEffect(()=>{
-    if (props.theme.theme === 'dark') { //kalo lah diset dari awal dark, toggleSwitch ny langsung ke switch
-      setChecked(false);
-    } else {
-      setChecked(true);
-    }
-  })
+    props.theme.theme === 'dark' ? setChecked(false) : setChecked(true) //kalo lah diset dari awal dark, toggleSwitch ny langsung ke switch
+    
+  }, [props.theme.theme])
   
   const switchTheme = () => {
-		const newTheme = props.theme.theme === 'light' ? 'dark' : 'light';
-    
-    if (newTheme === 'dark') {
-      setChecked(false);
-    } else {
-      setChecked(true);
-    }
-
-		props.theme.setTheme(newTheme);
+		props.theme.setTheme(props.theme.theme === 'light' ? 'dark' : 'light')
 	}
 
   return (
